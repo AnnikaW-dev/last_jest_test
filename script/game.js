84b273be46ac40da5e68f114ea2688352b3bb042
Annika Warm <warmannika@gmail.com>
2025-01-06 18:58:54 +0000

    More Advanced Tests - Part 1

--- script/game.js
+++ script/game.js
@@ -11,10 +11,17 @@ function newGame() {
     game.playerMoves = [];
     game.score = 0;
     showScore();
+    addTurn();
 };
+
+function addTurn() {
+    game.playerMoves = [];  // reset the players moves to 0 
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);  // Randomly add a button ID to the currentGame array
+    //showTurns();
+}
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
 };
 
-module.exports = { game, newGame, showScore };
+module.exports = { game, newGame, showScore, addTurn };
--- script/tests/game.test.js
+++ script/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore } = require("../game");
+const { game, newGame, showScore, addTurn } = require("../game");
 
 
 beforeAll(() => {
@@ -45,8 +45,8 @@ describe("newGame works correctly", () => {
     test("should clear the player moves array", () => {
         expect(game.playerMoves.length).toBe(0);
     });
-    test("should clear the computer sequence array", () => {
-        expect(game.currentGame.length).toBe(0);
+    test("Should be one move in the computer's game arry", () => {
+        expect(game.currentGame.length).toBe(1);
     });
     test("should display 0 for the element with id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
