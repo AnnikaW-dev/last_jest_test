e743b2de9ccb33d3016b3494723ce117158487b4
Annika Warm <warmannika@gmail.com>
2025-01-06 19:45:26 +0000

    More Advanced Tests - Part 2 js test is ok but test in browser doesn't work as expected

--- script/tests/game.test.js
+++ script/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore, addTurn } = require("../game");
+const { game, newGame, showScore, addTurn, lightsOn } = require("../game");
 
 
 beforeAll(() => {
@@ -45,10 +45,33 @@ describe("newGame works correctly", () => {
     test("should clear the player moves array", () => {
         expect(game.playerMoves.length).toBe(0);
     });
-    test("Should be one move in the computer's game arry", () => {
+    test("Should be one move in the computer's game array", () => {
         expect(game.currentGame.length).toBe(1);
     });
     test("should display 0 for the element with id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
+
+describe("gameplay works correctly", () => {
+    beforeEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+        addTurn();
+    });
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    });
+    test ("addTurn adds a new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    })
+    test("should add correct class to light up the buttons", () => {
+       let button = document.getElementById(game.currentGame[0]);
+       lightsOn(game.currentGame[0]);
+       expect(button.classList).toContain("light"); 
+    });
+})
