32fb83c66c974c19a89306114ff95d4018a9fe88
Annika Warm <warmannika@gmail.com>
2025-01-07 20:50:41 +0000

    Extending Our Tests the last lessons

--- script/game.js
+++ script/game.js
@@ -1,34 +1,56 @@
-
 let game = {
     currentGame: [],
-    score: 0,
     playerMoves: [],
-    choices: ["button1", "button2", "button3", "button4"],
+    score: 0,
+    turnNumber: 0,
+    choices: ["button1", "button2", "button3", "button4"]
 };
 
 function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
     game.score = 0;
+    for (let circle of document.getElementsByClassName("circle")) {
+        if (circle.getAttribute("data-listener") !== "true") {
+            circle.addEventListener("click", (e) => {
+                let move = e.target.getAttribute("id");
+                lightsOn(move);
+                game.playerMoves.push(move);
+                playerTurn();
+            });
+            circle.setAttribute("data-listener", "true");
+        }
+    }
     showScore();
     addTurn();
-};
+}
 
 function addTurn() {
-    game.playerMoves = [];  // reset the players moves to 0 
-    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);  // Randomly add a button ID to the currentGame array
-    //showTurns();
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    showTurns();
+}
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(function () {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
 }
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
-};
+}
 
-function lightsOn(circ) {
-    document.getElementById(circ).classList.add("light");
-    setTimeout(() => {
-        document.getElementById(circ).classList.remove("light");
-    }, 400)
-;}
-
-module.exports = { game, newGame, showScore, addTurn, lightsOn};
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
